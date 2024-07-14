50820aae2f62653a11a6651df3d08484b26799a8
aniimessh <animeshgupta2.002@gmail.com>
2024-07-14 11:44:21 +0530

    fix: logo developed into anchor tag, tag line font increased, bottom component change to sticky behaviour.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex md:justify-center md:items-center items-start h-screen">
       <div className="flex flex-col items-center gap-y-12">
         <h1 className="font-extrabold text-6xl text-center leading-tight">
           Create .Connect. Discover
         </h1>
-        <p className="text-center">
+        <p className="text-center text-xl font-bold">
           This is a Community of Coders{" "}
           <span className="text-red-800"> who are passionate</span> in Computer
           Science Field,
           <span className="text-yellow-400">
             {" "}
+            <br />
             Hunting for their next tech job
           </span>{" "}
           and are building a unique product.
