4be31d80055741315f265b7ca608896671711bbd
aniimessh <animeshgupta2.002@gmail.com>
2024-08-07 19:47:13 +0530

    feat: job details paged ui change

--- src/components/core/SimilarJob.tsx
+++ src/components/core/SimilarJob.tsx
@@ -4,7 +4,7 @@ import JobCard from "./JobCard";
 const SimilarJob = ({ similarJob }: any) => {
   return (
     <div>
-      <p className="text-3xl font-bold text-black">Apply For Similar Jobs:</p>
+      <p className="text-3xl font-bold text-white">Apply For Similar Jobs:</p>
       <div className="flex gap-5 mt-5 flex-wrap">
         {similarJob?.map((item: any) => {
           return (
