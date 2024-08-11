7d6aca892c00df41c96fb5bb2ad38629ecc2ca76
MilindGupta <milindgupta578@gmail.com>
2024-08-11 23:21:26 +0530

    CSS: Changes for Home Font Color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         </h1>
         <p className="text-center text-xl font-bold">
           This is a Community of Coders{" "}
-          <span className="text-red-800"> who are passionate</span> in Computer
+          <span className="text-red-500"> who are passionate</span> in Computer
           Science Field,
           <span className="text-yellow-400">
             {" "}
