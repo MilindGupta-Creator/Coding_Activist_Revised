c32d991570b557bcbf74dabe2e73f70b8a222233
MilindGupta <milindgupta578@gmail.com>
2025-03-01 19:40:00 +0530

    Added about me data linkedin

--- src/app/join-us/page.js
+++ src/app/join-us/page.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { FaWhatsapp, FaTelegramPlane, FaLinkedinIn } from "react-icons/fa";
+import profile_pic from "../../assets/img/profile_pic.jpeg"
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -14,14 +16,22 @@ const Home = () => {
         <Link href="https://t.me/codingactivist" target="_blank">
           <FaTelegramPlane className="bg-[#333333] p-5 rounded-lg" size={150} />
         </Link>
-        {/* <Link href="https://www.linkedin.com/in/milindguptaji/" target="_blank">
-          <img
-            src="https://codingactivist.com/static/media/profile-pic.569cb494286da3d4eac8.png"
-            alt="profile"
-            height={150}
-            width={150}
-          />
-        </Link> */}
+        <Link href="https://www.linkedin.com/in/milindguptaji/" target="_blank" className="mt-8">
+          {/* <div className="flex flex-col items-center">
+            <div className="bg-[#333333]  rounded-lg" style={{ width: "150px", height: "150px" }}>
+              <Image
+                src={profile_pic}
+                alt="profile"
+                height={150}
+                width={150}
+                className="rounded-lg mx-auto h-[150px]"
+              />
+            </div>
+            {/* <p className="mt-2 text-center font-medium">Developer</p> */}
+
+          <FaLinkedinIn className="bg-[#333333] p-5 rounded-lg" size={150} />
+          <p className="mt-2 text-center font-medium">Creator & Developer</p>
+        </Link>
       </div>
     </div>
   );
